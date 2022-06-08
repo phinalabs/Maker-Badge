b3be8c1176e08cb3661acfb53dbded3839eddaba
mevolkan <nzaro19@gmail.com>
2022-06-09 02:38:33 +0300

    smantic ui working

--- App/Front-End/src/index.js
+++ App/Front-End/src/index.js
@@ -1,8 +1,12 @@
+import 'semantic-ui-css/semantic.min.css'
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
