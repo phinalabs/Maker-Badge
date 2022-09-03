b3a6d0519f9f802b9fbad69e82040933e9f902e1
volkan <16579273+mevolkan@users.noreply.github.com>
2022-09-04 02:02:40 +0300

    Ui additions

--- App/Front-End/pages/badge.js
+++ App/Front-End/pages/badge.js
@@ -2,11 +2,63 @@ import EventsCard from "../components/events"
 
 function Badge() {
     return (
-      <>
-      <div>Badge</div>
-      <EventsCard />
-      </>
+        <div className="container mx-auto px-4 sm:px-6">
+            <div>Badge</div>
+                <div className="bg-white rounded-md bg-gray-800 shadow-lg">
+                    <div className="md:flex px-4 leading-none max-w-4xl">
+                        <div className="flex-none ">
+                            <img
+                                src="https://creativereview.imgix.net/content/uploads/2019/12/joker_full.jpg?auto=compress,format&q=60&w=1012&h=1500"
+                                alt="pic"
+                                className="h-72 w-56 rounded-md shadow-2xl transform -translate-y-4 border-4 border-gray-300 shadow-lg"
+                            />
+                        </div>
+
+                        <div className="flex-col text-gray-300">
+
+                            <p className="pt-4 text-2xl font-bold">Joker (2020)</p>
+                            <hr className="hr-text" data-content="" />
+                                <div className="text-md flex justify-between px-4 my-2">
+                                    <span className="font-bold">2h 2min | Crime, Drama, Thriller</span>
+                                    <span className="font-bold"></span>
+                                </div>
+                                <p className="hidden md:block px-4 my-4 text-sm text-left">In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker. </p>
+
+                                <p className="flex text-md px-4 my-2">
+                                    Rating: 9.0/10
+                                    <span className="font-bold px-2">|</span>
+                                    Mood: Dark
+                                </p>
+
+                                <div className="text-xs">
+                                    <button type="button" className="border border-gray-400 text-gray-400 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-900 focus:outline-none focus:shadow-outline">TRAILER</button>
+
+                                    <button type="button" className="border border-gray-400 text-gray-400 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-900 focus:outline-none focus:shadow-outline">IMDB</button>
+
+                                    <button type="button" className="border border-gray-400 text-gray-400 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-900 focus:outline-none focus:shadow-outline">AMAZON</button>
+                                </div>
+         <p>ICON BTNS</p>
+
+                        </div>
+                    </div>
+                    <div className="flex justify-between items-center px-4 mb-4 w-full">
+                        <div className="flex">
+                            <i className="material-icons mr-2 text-red-600">favorite_border</i>
+                            <i className="material-icons text-blue-600">remove_red_eye</i>
+                        </div>
+                        <div className="flex">
+                            <i className="material-icons ml-2 text-yellow-600">sentiment_very_satisfied</i>
+                            <i className="material-icons ml-2 text-yellow-600">sentiment_neutral</i>
+                            <i className="material-icons ml-2 text-yellow-600">sentiment_very_dissatisfied</i>
+                            <i className="material-icons ml-2 text-yellow-600">star_outline</i>
+                            <i className="material-icons ml-2 text-yellow-600">star_half</i>
+                            <i className="material-icons ml-2 text-yellow-600">star</i>
+
+                        </div>
+                    </div>
+                </div>
+        </div>
     )
-  }
-  
-  export default Badge
+}
+
+export default Badge
---
+++ App/Front-End/pages/subscirbe.js
@@ -0,0 +1,69 @@
+function Subscribe() {
+    return (
+      <div>
+        <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <div class="flex flex-col justify-between lg:flex-row">
+    <div class="mb-12 lg:max-w-lg lg:pr-5 lg:mb-0">
+      <div class="max-w-xl mb-6">
+        <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+          The quick, brown fox<br class="hidden md:block" />
+          jumps over
+          <span class="inline-block text-deep-purple-accent-400">a lazy dog</span>
+        </h2>
+        <p class="text-base text-gray-700 md:text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. explicabo.
+        </p>
+      </div>
+      <hr class="mb-6 border-gray-300" />
+      <div class="flex">
+        <a href="/" aria-label="Play Song" class="mr-3">
+          <div class="flex items-center justify-center w-10 h-10 text-white transition duration-300 transform rounded-full shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 hover:scale-110">
+            <svg class="w-6" fill="currentColor" viewBox="0 0 24 24">
+              <path
+                d="M16.53,11.152l-8-5C8.221,5.958,7.833,5.949,7.515,6.125C7.197,6.302,7,6.636,7,7v10 c0,0.364,0.197,0.698,0.515,0.875C7.667,17.958,7.833,18,8,18c0.184,0,0.368-0.051,0.53-0.152l8-5C16.822,12.665,17,12.345,17,12 S16.822,11.335,16.53,11.152z"
+              ></path>
+            </svg>
+          </div>
+        </a>
+        <div class="flex flex-col">
+          <div class="text-sm font-semibold">Rich the kid &amp; Famous Dex</div>
+          <div class="text-xs text-gray-700">Rich Forever Intro</div>
+        </div>
+      </div>
+    </div>
+    <div class="px-5 pt-6 pb-5 text-center border border-gray-300 rounded lg:w-2/5">
+      <div class="mb-5 font-semibold">Create an account</div>
+      <div class="flex justify-center w-full mb-3">
+        <a
+          href="/"
+          class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+        >
+          <div class="flex items-center">
+            <div class="mr-3 font-semibold text-white">Login with Facebook</div>
+            <svg viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+              <path
+                d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"
+              ></path>
+            </svg>
+          </div>
+        </a>
+      </div>
+      <p class="max-w-md px-5 mb-3 text-xs text-gray-600 sm:text-sm md:mb-5">
+        Sed ut unde omnis iste natus accusantium doloremque laudantium omnis iste.
+      </p>
+      <div class="flex items-center w-full mb-5">
+        <hr class="flex-1 border-gray-300" />
+        <div class="px-3 text-xs text-gray-500 sm:text-sm">or</div>
+        <hr class="flex-1 border-gray-300" />
+      </div>
+      <a href="/" class="inline-flex items-center justify-center w-full h-12 px-6 font-semibold transition duration-200 bg-white border border-gray-300 rounded md:w-auto hover:bg-gray-100 focus:shadow-outline focus:outline-none">
+        Sign Up with Email
+      </a>
+    </div>
+  </div>
+</div>
+      </div>
+    )
+  }
+  
+  export default Subscribe
