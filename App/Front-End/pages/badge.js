154dc7953b36e124c2ca0e8895648502858fa66e
volkan <16579273+mevolkan@users.noreply.github.com>
2022-09-04 00:38:14 +0300

    changes to main

---
+++ App/Front-End/pages/badge.js
@@ -0,0 +1,12 @@
+import EventsCard from "../components/events"
+
+function Badge() {
+    return (
+      <>
+      <div>Badge</div>
+      <EventsCard />
+      </>
+    )
+  }
+  
+  export default Badge
