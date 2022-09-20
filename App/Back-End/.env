98a7c45f459531ea1de902edcbf631f960f0b351
volkan <16579273+mevolkan@users.noreply.github.com>
2022-09-21 02:51:23 +0300

    secure variables

---
+++ App/Back-End/.env
@@ -0,0 +1,3 @@
+PORT=5000
+MONGO_DB_URL=mongodb://localhost:27017
+MONGO_DB_DBNAME=store
--- App/Back-End/index.js
+++ App/Back-End/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const  cors = require('cors');
+const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('./config/database');
 const pkg = require('./package.json');
