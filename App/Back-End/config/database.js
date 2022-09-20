8a570083e96145fffc0f49a1a960076ca60f784f
volkan <16579273+mevolkan@users.noreply.github.com>
2022-09-21 02:51:23 +0300

    secure variables

--- App/Back-End/config/database.js
+++ App/Back-End/config/database.js
@@ -1,14 +1,26 @@
+
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+dotenv.config()
 
-// Config DB Connection
-const mongoDB = 'mongodb://localhost/makerBadge';
+const DB_URL = `${process.env.MONGO_DB_URL}/${process.env.MONGO_DB_DBNAME}`;
+const PORT = process.env.PORT;
 
-mongoose.connect(mongoDB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-mongoose.Promise = global.Promise;
+mongoose.connect(DB_URL, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+  }, () => {
+    console.log(DB_URL);
+    console.log(`DB up and running`);
+  });
+   mongoose.Promise = global.Promise;
 
+// mongoose.connect(mongoDB, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+// mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
+
