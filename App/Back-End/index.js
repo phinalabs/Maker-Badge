4fdfa144a8e95071aa6f2808ef590783e0c23a39
volkan <16579273+mevolkan@users.noreply.github.com>
2022-10-14 01:17:11 +0300

    secure db access urls

--- App/Back-End/index.js
+++ App/Back-End/index.js
@@ -1,20 +1,17 @@
+const config = require('./database.js');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const mongoose = require('./config/database');
 const pkg = require('./package.json');
+const mongoose = require('mongoose');
+const mode = '';
+
 
 const authRoutes = require('./routes/auth.routes');
 const badgeRoutes = require('./routes/badge.routes');
 const eventRoutes = require('./routes/event.routes')
 
-
 const app = express();
-
-
-// DB settings
-mongoose.connection.on('error', console.error.bind(console, 'DB Connection Errror'));
-
 
 // Settings
 app.set('pkg', pkg);
@@ -25,12 +22,25 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/badges', badgeRoutes);
 app.use('/api/events', eventRoutes);
 
+var path = require('path');
+
+console.log(`MONGO_URI= ${config.MONGO_URI}`);
+
+mongoose.connect(`${config.MONGO_URI}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to mongo database");
+    })
+    .catch((err) => {
+        console.log("Error connecting mongo database", err);
+        process.exit(1);
+    });
+
+var db = mongoose.connection;
 
 // Welcome Route
 app.get('/', (req, res) => {
@@ -42,7 +52,6 @@ app.get('/', (req, res) => {
     })
 })
 
-
-app.listen(3000, () => {
-    console.log('Server running on port: 3000')
-});
+app.listen(config.PORT, config.HOST, () => {
+    console.log(`APP LISTENING ON http://127.0.0.1:${config.PORT}`);
+})
